cace3c4f8b86a60cc1814f3931a9ed3ed5037926
Andrea Bardone <a.bardone@reply.it>
2018-12-06 19:25:12 +0100

    aa
    a

--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -7,6 +7,8 @@ class ProductsList extends Component {
     this.state = {
       products: [{name:'prodotto1',count:10},{name:'prodotto2',count:1}]
     };
+  
+
   }
 
   componentDidMount = () => {
@@ -19,6 +21,15 @@ class ProductsList extends Component {
         <div className="card-header">Product List</div>
         <div className="card-body">
           <ul className="list-group">
+
+{this.state.products.map = ((product,index) => {
+      return (
+            <li className="list-group-item" key={index}>{product.name}>
+              <span className="badge badge-pill badge-success">{product.count}</span>
+            </li> 
+        );
+    })}
+
             <li className="list-group-item">
               Product 1
               <span className="badge badge-pill badge-success">10</span>
